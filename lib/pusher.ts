51a2171d156d3654512d7e93491a76800b0fb0d6
kaguriuki001 <jkagurukariuki01@gmail.com>
2026-09-18 15:58:56 +0000

    Fix pusher.ts

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -1,1 +1,17 @@
-DONE pusher libcat
+import PusherServer from "pusher";
+import PusherClient from "pusher-js";
+
+export const pusherServer = new PusherServer({
+  appId: process.env.PUSHER_APP_ID || "",
+  key: process.env.PUSHER_KEY || "",
+  secret: process.env.PUSHER_SECRET || "",
+  cluster: process.env.PUSHER_CLUSTER || "eu",
+  useTLS: true
+});
+
+export function getPusherClient() {
+  if (typeof window === "undefined") return null;
+  return new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY || "", {
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || "eu"
+  });
+}
